a48342ba5d8f43fa8615ac196a8b9d45645d2468
Amith Sai <amithsai78@gmail.com>
2021-03-11 12:08:48 +0530

    Added Multiple Consumers and producers

--- src/main/java/com/project/kafka/service/ConsumerService.java
+++ src/main/java/com/project/kafka/service/ConsumerService.java
@@ -1,23 +1,27 @@
 package com.project.kafka.service;
 
 import com.project.kafka.avro.Category;
+import com.project.kafka.avro.Product;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class ConsumerService {
 
-    @KafkaListener(topics = "category", groupId = "group_id")
+    @KafkaListener(topics = "category", groupId = "category_id")
     public void consumeMessage(Category category) {
         log.info("Consuming Message: ===========> {}", category);
     }
-//
-//    @KafkaListener(topics = "product", groupId = "group_id")
-//    public void consumeProductMessage(Product product) {
-//        log.info("Consuming message: =============?> {}", product);
-//    }
+
+    @KafkaListener(topics = "product",  groupId = "product_id")
+    public void consumeProductMessage(Product product) {
+        log.info("Consuming message: =============?> {}", product);
+    }
 
 
 }
--- src/main/java/com/project/kafka/service/ProducerService.java
+++ src/main/java/com/project/kafka/service/ProducerService.java
@@ -5,7 +5,9 @@ import com.project.kafka.avro.Product;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Slf4j
 @Service
@@ -24,11 +26,31 @@ public class ProducerService {
 
     public void sendProduct(Product product) {
         log.info("Producing message =============> {}", product.toString());
-        this.productKafkaTemplate.send(PRODUCTTOPIC, product);
+        this.productKafkaTemplate.send(PRODUCTTOPIC, product).addCallback(new ListenableFutureCallback<SendResult<String, Product>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.info("Exception Occurred: {}", ex.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Product> result) {
+                log.info("Successfully Produced message {}", result.getProducerRecord().toString());
+            }
+        });
     }
 
     public void sendCategory(Category category) {
         log.info("Producing message =============> {}", category.toString());
-        this.categoryKafkaTemplate.send(CATEGORYTOPIC, category);
+        this.categoryKafkaTemplate.send(CATEGORYTOPIC, category).addCallback(new ListenableFutureCallback<SendResult<String, Category>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.info("Exception Occurred: {}", ex.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, Category> result) {
+                log.info("Successfully Produced message {}", result.getProducerRecord().toString());
+            }
+        });
     }
 }
